4c8fa76bf119ac783eb524c68fd16ac202fb761b
Jide Olawoye <g00364685@gmit.ie>
2021-02-21 21:15:30 +0000

    Trying carousel

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,14 +1,58 @@
 
 import React from 'react';
+import { CarouselItem } from 'react-bootstrap';
+import Carousel from "react-bootstrap/Carousel";
+import "react-bootstrap-carousel/dist/react-bootstrap-carousel.css";
+
 
 export class Home extends React.Component {
 
     render() {
         return (
             <div>
-                <a href ="/income">Income Pg</a><br/>
-                <a href ="/expense">Expense Pg</a><br/>
-                <a href ="/goal">Goal pg</a><br/>
+
+                <Carousel>
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100"
+                            src="holder.js/800x400?text=First slide&bg=373940"
+                            alt="First slide"
+                        />
+                        <Carousel.Caption>
+                            <h3>Welcome</h3>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100"
+                            src="holder.js/800x400?text=Second slide&bg=282c34"
+                            alt="Second slide"
+                        />
+
+                        <Carousel.Caption>
+                            <h3>Manage your money source</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100"
+                            src="holder.js/800x400?text=Third slide&bg=20232a"
+                            alt="Third slide"
+                        />
+
+                        <Carousel.Caption>
+                            <h3>Get to your goal</h3>
+                            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                </Carousel>
+                
+
+                <a href="/income">Income Pg</a><br />
+                <a href="/expense">Expense Pg</a><br />
+                <a href="/goal">Goal pg</a><br />
+
             </div>
         )
     }
